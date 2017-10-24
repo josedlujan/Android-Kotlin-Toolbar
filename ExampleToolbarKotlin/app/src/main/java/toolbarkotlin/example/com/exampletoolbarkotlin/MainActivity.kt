cda3532382af7eaf72af9a5ee5e51f082a858722
josedlujan <josedlujan@gmail.com>
2017-10-23 20:02:12 -0500

    initial commit

---
+++ ExampleToolbarKotlin/app/src/main/java/toolbarkotlin/example/com/exampletoolbarkotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package toolbarkotlin.example.com.exampletoolbarkotlin
+
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar:Toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        toolbar.setTitle("Titulo toolbar")
+    }
+}
